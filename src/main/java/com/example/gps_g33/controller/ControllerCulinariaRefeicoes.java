11b41766635cf251df5105c192349bef6e394f5c
Daniel Bravo <daniel8bravo@gmail.com>
2023-11-03 19:01:54 +0000

    Tasks 2 e 6-US17

--- src/main/java/com/example/gps_g33/controller/ControllerCulinariaRefeicoes.java
+++ src/main/java/com/example/gps_g33/controller/ControllerCulinariaRefeicoes.java
@@ -18,6 +18,7 @@ import java.util.Date;
 
 public class ControllerCulinariaRefeicoes{
     public Button buttonToAddRefeicao;
+    public Button buttonToEditRefeicao
     public Button buttonToRefeicoes;
     public Button buttonToResidentes;
     public StackPane contentArea;
@@ -55,6 +56,28 @@ public class ControllerCulinariaRefeicoes{
             Stage popupStage = new Stage();
             popupStage.initModality(Modality.APPLICATION_MODAL); // Torna a janela pop-up modal
             popupStage.setTitle("Adicionar Refeição"); // Defina o título da janela pop-up
+            popupStage.setScene(popupScene);
+
+            // Mostrar a janela pop-up
+            popupStage.showAndWait(); // Espere até que a janela pop-up seja fechada
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void handleToEditRefeicao() {
+        try {
+            // Carregar o FXML da janela pop-up
+            FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource("views/depCulinaria/Culinaria_EditRefeicao.fxml"));
+            Parent popupRoot = loader.load();
+
+            // Criar uma nova cena com a janela pop-up
+            Scene popupScene = new Scene(popupRoot);
+
+            // Criar um novo palco (Stage) para a janela pop-up
+            Stage popupStage = new Stage();
+            popupStage.initModality(Modality.APPLICATION_MODAL); // Torna a janela pop-up modal
+            popupStage.setTitle("Editar Refeição"); // Defina o título da janela pop-up
             popupStage.setScene(popupScene);
 
             // Mostrar a janela pop-up
