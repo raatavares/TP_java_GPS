6a11029c3e844ca998dfe5ed6fb6c42963537972
ShorMario <mariofrancisco.lourenco@gmail.com>
2023-12-11 22:42:27 +0000

    test pipe

--- src/main/java/com/example/gps_g33/HelloApplication.java
+++ src/main/java/com/example/gps_g33/HelloApplication.java
@@ -1,17 +1,12 @@
 package com.example.gps_g33;
 
 import com.example.gps_g33.modelos.Data;
-import com.example.gps_g33.modelos.Funcionario;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
-import java.util.Hashtable;
-import java.util.Objects;
-
-import javafx.application.Platform;
 
 public class HelloApplication extends Application {
     public Data data;
@@ -22,7 +17,7 @@ public class HelloApplication extends Application {
 
         // Verifica se há credenciais salvas
         if (data.loadCredentials() != null) {
-            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("views/login/Login.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/views/login/Login.fxml"));
             Parent root = fxmlLoader.load();
             Scene scene = new Scene(root, 500, 250);
             scene.getStylesheets().add(getClass().getResource("css/style.css").toExternalForm());
